26275851ed1de2b8fd89ef7e83977b16e9fc5321
mganopolsky <ganopolsky.marina@gmail.com>
2022-06-16 07:45:20 -0500

    first draft of monthly log function

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,71 +3,87 @@ import { useState } from "react";
 import DatePickerSelector from "./DatePickerSelector";
 import MoodSelect from "./MoodSelect";
 import PeriodSelect from "./PeriodSelect";
+import MonthlyLog from "./MonthlyLog";
 import { Button } from "@mui/material";
 import { ethers } from "ethers";
 import abi from "../utils/OonMoon.json";
 
 export default function Container() {
-    const [datePicker, setDatePicker] = useState("")
-    const [moodPicker, setMoodPicker] = useState(0)
-    const [cyclePicker, setCyclePicker] = useState(0)
-    const contractAddress = "0x4f6977502F7bd2E8Ff128781aAb0a2ad26EBE7dE"
-    const abiContract = abi.abi
+  const [datePicker, setDatePicker] = useState("");
+  const [moodPicker, setMoodPicker] = useState(0);
+  const [cyclePicker, setCyclePicker] = useState(0);
+  const contractAddress = "0x4f6977502F7bd2E8Ff128781aAb0a2ad26EBE7dE";
+  const abiContract = abi.abi;
 
+  const sendAllDataOnSave = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const sendDailyLog = new ethers.Contract(
+          contractAddress,
+          abiContract,
+          signer
+        );
+        const dateSelected = new Date(datePicker);
 
-    const sendAllDataOnSave = async () => {
-        try {
-            const { ethereum } = window;
-            if (ethereum) {
+        let dailyLogTxn = await sendDailyLog.logDailyData(
+          dateSelected.getFullYear(),
+          dateSelected.getMonth(),
+          dateSelected.getDay(),
+          moodPicker,
+          cyclePicker,
+          { gasLimit: 300000 }
+        );
 
-                const provider = new ethers.providers.Web3Provider(ethereum);
-                const signer = provider.getSigner()
-                const sendDailyLog = new ethers.Contract(contractAddress, abiContract, signer)
-                const dateSelected = new Date(datePicker)
-
-                let dailyLogTxn = await sendDailyLog.logDailyData(dateSelected.getFullYear(), dateSelected.getMonth(), dateSelected.getDay(), moodPicker, cyclePicker, { gasLimit: 300000 })
-
-                console.log("dailyLog", dailyLogTxn)
-
-
-            } else {
-                console.log("No ethereum object!")
-            }
-        } catch (error) {
-            return ("Please make sure you've selected all options!")
-        }
+        console.log("dailyLog", dailyLogTxn);
+      } else {
+        console.log("No ethereum object!");
+      }
+    } catch (error) {
+      return "Please make sure you've selected all options!";
     }
+  };
 
-    function onDatePicker(value) {
-        console.log("onDatePicker :", value);
-        setDatePicker(value)
+  function onDatePicker(value) {
+    console.log("onDatePicker :", value);
+    setDatePicker(value);
+  }
 
-    }
+  function onMoodPicker(value) {
+    console.log("onMoodPicker :", value);
 
-    function onMoodPicker(value) {
-        console.log("onMoodPicker :", value);
+    setMoodPicker(value);
+  }
 
-        setMoodPicker(value)
-    }
+  function onCyclePicker(value) {
+    console.log("onCyclePicker :", value);
 
-    function onCyclePicker(value) {
-        console.log("onCyclePicker :", value);
+    setCyclePicker(value);
+  }
 
-        setCyclePicker(value)
-
-    }
-
-    return (
-        <>
-            <div className={"calendar"}>
-                <DatePickerSelector onHappyTime={onDatePicker} />
-            </div>
-            <div className={"big-div"}>
-                <MoodSelect onMoodPicker={onMoodPicker} />
-                <PeriodSelect onCyclePicker={onCyclePicker} />
-                <Button className={"save_button"} variant="contained" color={"secondary"} onClick={sendAllDataOnSave} size={"large"}
-                >Save</Button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={"calendar"}>
+        <DatePickerSelector onHappyTime={onDatePicker} />
+      </div>
+      <div className={"big-div"}>
+        <MoodSelect onMoodPicker={onMoodPicker} />
+        <PeriodSelect onCyclePicker={onCyclePicker} />
+        <Button
+          className={"save_button"}
+          variant="contained"
+          color={"secondary"}
+          onClick={sendAllDataOnSave}
+          size={"large"}
+        >
+          Save
+        </Button>
+      </div>
+      <div>
+        <MonthlyLog />
+      </div>
+    </>
+  );
 }
--- src/components/MonthlyLog.js
+++ src/components/MonthlyLog.js
@@ -1,9 +1,61 @@
-import * as React from 'react';
-import { Button } from '@mui/material';
+import * as React from "react";
+import { Button } from "@mui/material";
+import abi from "../utils/OonMoon.json";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 export default function MonthlyLog() {
+  const [monthlyResults, setMonthlyResults] = useState("");
+  const contractAddress = "0x4f6977502F7bd2E8Ff128781aAb0a2ad26EBE7dE";
+  const abiContract = abi.abi;
+
+  function onMonthlyResults(value) {
+    console.log("onMonthlyResults :", value);
+    document.getElementById("monthlyPeriodView").innerText = value;
+    setMonthlyResults(value);
+  }
+
+  const getMonthlyData = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const monthlyView = new ethers.Contract(
+          contractAddress,
+          abiContract,
+          signer
+        );
+        const year = 2022;
+        const month = 5;
+
+        let monthlyViewTxn = await monthlyView.getMyMonthlyHistory(year, month);
+
+        console.log("dailyLog output is:", monthlyViewTxn);
+
+        setMonthlyResults(monthlyViewTxn);
+      } else {
+        console.log("No ethereum object!");
+      }
+    } catch (error) {
+      console.log(`error: ${error}`);
+      return "Please make sure you've selected all options!";
+    }
+  };
 
   return (
-    <div></div>
+    <div>
+      <div id="monthlyPeriodView"></div>
+
+      <Button
+        className={"save_button"}
+        variant="contained"
+        color={"secondary"}
+        onClick={getMonthlyData}
+        size={"large"}
+      >
+        Monthly View
+      </Button>
+    </div>
   );
 }
